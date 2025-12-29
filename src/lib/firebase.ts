332a4e350ef05522e9444c4bd2b01871c5a93e61
Kisaan InfoTech <kisaaninfotechltd@gmail.com>
2025-12-29 11:37:40 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyASSCe639w3ixIXMmRpD_diEaaILFoYLGs",
+  authDomain: "studio-2243883462-63f35.firebaseapp.com",
+  projectId: "studio-2243883462-63f35",
+  storageBucket: "studio-2243883462-63f35.appspot.com",
+  messagingSenderId: "764383211778",
+  appId: "1:764383211778:web:6a1ba85f88f8075d26d21b",
+  measurementId: ""
 };
 
 // Initialize Firebase
